827e7fa737c0ab5b1460941a025c949e8daa6cf0
Riviox <100956266+riviox@users.noreply.github.com>
2023-12-24 15:35:24 +0100

    added autorun

--- main.py
+++ main.py
@@ -6,15 +6,44 @@ class CommandModule:
         pass
 
 modules_dir = "modules"
+autorun_dir = os.path.join(modules_dir, "autorun")
+
 modules = [module[:-3] for module in os.listdir(modules_dir) if module.endswith(".py") and module[:-3] != "__init__"]
+autorun_modules = [module[:-3] for module in os.listdir(autorun_dir) if module.endswith(".py")]
+
+all_modules = modules + ["autorun\\" + module for module in autorun_modules]
 
 module_objects = {}
 
 for module_name in modules:
-    module_path = f"{modules_dir}.{module_name}"
-    module = importlib.import_module(module_path)
-    module_class = getattr(module, module_name.capitalize())
-    module_objects[module_name] = module_class()
+    try:
+        module_path = f"{modules_dir}.{module_name}"
+        file_path = os.path.join(modules_dir, f"{module_name}.py")
+        print(f"Attempting to load module: {file_path}")
+
+        spec = importlib.util.spec_from_file_location(module_name, file_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+
+        
+        module_objects[module_name] = module
+    except ModuleNotFoundError as e:
+        print(f"Error importing module {module_name}: {e}")
+
+
+for module_name in autorun_modules:
+    try:
+        module_path = f"{modules_dir}.autorun.{module_name}"
+        file_path = os.path.join(autorun_dir, f"{module_name}.py")
+
+        spec = importlib.util.spec_from_file_location(module_name, file_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+
+        
+        module_objects[module_name] = module
+    except ModuleNotFoundError as e:
+        print(f"Error importing autorun module {module_name}: {e}")
 
 while True:
     pwd = os.getcwd()
---
+++ modules/autorun/hello.py
@@ -0,0 +1,3 @@
+from modules.colors import Colors
+
+print(f'{Colors.BLUE} BOOTED {Colors.GREEN} BaobabOS {Colors.RESET}')
